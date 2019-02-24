734094aeb2a59d2e28197c6d7919a5910c39fbbf
zhangchaojie <1098626505@qq.com>
2019-02-24 14:43:11 +0800

    修改文档

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
+  publicPath: isDevelopment ? '/' : '/vue-image-pro/',
   outputDir: path.resolve(__dirname, './docs/'),
   css: { extract: isDevelopment },
   configureWebpack: {
